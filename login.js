e1026fc6be0efe18c43381a0c99a9f3ae44884d3
VidehiBavishi <63579878+VidehiBavishi@users.noreply.github.com>
2021-05-06 14:18:44 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,8 @@
+function login(){
+
+    var userEmail= document.getElementById("email").value;
+    var userPassword= document.getElementById("password").value;
+
+    window.alert(userEmail + " " + userPassword);
+
+}
